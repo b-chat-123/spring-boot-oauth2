126866af836e67f4d7515d17531ed940238f3b5f
Bidhan Chattopadhyay <b_chat_123@yahoo.co.in>
2023-07-14 00:38:20 +0530

    Initial commit all

---
+++ bidhan-spring-boot-oauth2/src/main/java/com/bidhanc/BidhanSpringBootOauth2Application.java
@@ -0,0 +1,13 @@
+package com.bidhanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidhanSpringBootOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidhanSpringBootOauth2Application.class, args);
+	}
+
+}
---
+++ bidhan-spring-boot-oauth2/src/test/java/com/bidhanc/BidhanSpringBootOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidhanc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidhanSpringBootOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
